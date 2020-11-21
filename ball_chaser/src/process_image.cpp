7d2f273a677ea592c9b67e31fcfcbbfa20bc47d3
George Dzhgarkava <dg@roommediagroup.com>
2020-11-21 19:41:03 +0000

    Fixed the for loop

--- ball_chaser/src/process_image.cpp
+++ ball_chaser/src/process_image.cpp
@@ -28,7 +28,7 @@ void process_image_callback(const sensor_msgs::Image img)
     int left_ball_side = -1;
     int right_ball_side = -1;
 
-    for (int i = 0; i < img.height * img.step - 2; i++) {
+    for (int i = 0; i < img.height * img.step - 2; i+=3) {
         if (img.data[i] == white_pixel && img.data[i+1] == white_pixel && img.data[i+2] == white_pixel)
         {
             target = i % img.step;
